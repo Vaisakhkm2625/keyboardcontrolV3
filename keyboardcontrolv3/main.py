1b859b641f5913da6005da0b7e5914820367c6d2
Vaisakh K M <vaisakhkm2625@gmail.com>
2023-12-05 09:26:28 +0530

    color selector

--- keyboardcontrolv3/main.py
+++ keyboardcontrolv3/main.py
@@ -2,11 +2,12 @@
 # Import statement
 import sys,json
 from time import process_time_ns
+from PyQt6.QtCore import pyqtSignal
 
-from PyQt6.QtWidgets import QApplication, QHBoxLayout, QVBoxLayout, QLineEdit, QListWidget, QListWidgetItem, QMainWindow, QPushButton, QWidget
-from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import QApplication, QHBoxLayout, QVBoxLayout, QLineEdit, QListWidget, QListWidgetItem, QMainWindow, QPushButton, QWidget, QMenu
+from PyQt6.QtGui import QAction, QIcon
 
-from qt_material import apply_stylesheet
+from qt_material import QtStyleTools, apply_stylesheet,list_themes
 
 from yapsy.PluginManager import PluginManager
 from plugin_categories.action_plugin import Action 
@@ -245,15 +246,33 @@ class Application(QApplication):
     def __init__(self, argv):
         super().__init__(argv)
 
-        apply_stylesheet(self, theme='dark_teal.xml')
+        #apply_stylesheet(self, theme='dark_teal.xml')
+        apply_stylesheet(self, theme='light_green.xml')
+
 
         self.window = MainWindow()
+        self.window.themeSignal.connect(self.apply_theme)
         self.window.show()
+
+        self.extra = { # Font
+            'font_family': 'monoespace',
+            'font_size': '20px',
+            'color': 'white',
+        }
+
+        apply_stylesheet(self, theme="dark_teal.xml")
+
+    def apply_theme(self,theme):
+
+        apply_stylesheet(self, theme=theme,invert_secondary=True,extra=self.extra)
+        
 
 
 class MainWindow(QMainWindow):
 
     action_plugin_list = []
+
+    themeSignal = pyqtSignal(str)
 
     def __init__(self):
         super().__init__()
@@ -263,6 +282,39 @@ class MainWindow(QMainWindow):
 
         #self.ui.apply.clicked.connect(self.run_some)
         self.manager = Manager(self.ui)
+        self.add_style_menu()
+
+
+    def add_style_menu(self):
+        # Create the main menu bar
+        menubar = self.menuBar()
+
+        # Create a menu for colors
+        color_menu = QMenu('Select Color', self)
+
+        # List of colors
+        #colors = ["red", "green", "blue", "black", "white", "red"]
+
+        colors = list_themes()
+
+        # Create actions for each color
+        color_actions = [QAction(color.replace("_","-").replace(".xml",""), self) for color in colors]
+
+        # Add color actions to the color menu
+        color_menu.addActions(color_actions)
+
+        # Connect each color action to the colorSelected method
+        for action in color_actions:
+            action.triggered.connect(self.colorSelected)
+
+        # Add the color menu to the main menu bar
+        menubar.addMenu(color_menu)
+
+    def colorSelected(self):
+        # Get the text of the triggered action (color)
+        selected_color = self.sender().text().replace("-","_")+".xml"
+        print(f'Selected color: {selected_color}')
+        self.themeSignal.emit(selected_color)
 
 
 
