9172c4d9e6b94b37b168b7c3e16323e15bfa6057
vaisakh <vaisakhkm2625@gmail.com>
2023-12-07 21:31:47 +0000

    keyboard plugins working

--- keyboardcontrolv3/plugins/keyboard_event/keyboard_plugin.py
+++ keyboardcontrolv3/plugins/keyboard_event/keyboard_plugin.py
@@ -1,27 +1,48 @@
 
-from PyQt6.QtWidgets import  QKeySequenceEdit, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import QKeySequenceEdit, QVBoxLayout, QWidget, QLabel
 from plugin_categories.event_plugin import Event
+import keyboard
+
+from PyQt6.QtGui import QKeySequence
+from PyQt6.QtCore import QThread
+from PyQt6.QtCore import pyqtSignal
+
+
+class KeyboardWorker(QThread):
+
+    run_function_signal = pyqtSignal()
+
+    def __init__(self, keyboard):
+        super().__init__()
+        self.keyboard = keyboard
+
+    def run(self):
+        self.keyboard.run()
+
 
 class KeyboardUiWidget(QWidget):
 
     def __init__(self):
         super().__init__()
         self.data = {}
-        self.keyedit = QKeySequenceEdit()
+        self.keyboard_edit = QKeySequenceEdit()
+        self.label = QLabel()
 
         self.vbox = QVBoxLayout()
-        self.vbox.addWidget(self.keyedit)
+        self.vbox.addWidget(self.label)
+        self.vbox.addWidget(self.keyboard_edit)
 
         self.setLayout(self.vbox)
 
     def get_ui_data(self):
-        self.data["shortcut"] = self.keyedit.keySequence().toString()
+        self.data["shortcut"] = self.keyboard_edit.keySequence().toString()
         return self.data
 
-    def set_ui_data(self,data):
+    def set_ui_data(self, data):
         self.data = data
-        print("setting keyboard data",self.data)
-        self.keyedit.setKeySequence(self.data["shortcut"])
+        print("setting keyboard data", self.data)
+        self.label.setText(self.data["shortcut"])
+        self.keyboard_edit.setKeySequence(QKeySequence(self.data["shortcut"]))
 
 
 class KeyboardEventPlugin(Event):
@@ -34,15 +55,20 @@ class KeyboardEventPlugin(Event):
         return KeyboardUiWidget
 
     def set_data_mappings(self, id_mappings=[]):
-        #self.key_mappings.append({"id":data})
+        # self.key_mappings.append({"id":data})
         self.key_mappings = id_mappings
-        print("keyboard mapping ->",self.key_mappings)
-        
+        print("keyboard mapping ->", self.key_mappings)
 
-    def start_event_listener(self,callback):
+    def start_event_listener(self, callback_func):
         # keyboard press ed > callback
         print("starting keyboard listenr")
 
+        for keymap in self.key_mappings:
+            print(keymap)
+            keyboard.add_hotkey(
+                # keymap["data"]["shortcut"], print, args=('triggered', 'hotkey'))
+                keymap["data"]["shortcut"], callback_func, args=(keymap["item"],))
 
     def stop_event_listener(self):
         print("stoping keyboard listenr")
+
